4c041b84ed2bafa32f782965f9cb4ed08e5e0cc1
mush0000 <mashroom0825@gmail.com>
2023-11-13 11:26:51 +0900

    P104タプル

---
+++ code2_17.py
@@ -0,0 +1,7 @@
+scores = (70,80,55)
+print(scores)
+print(scores[0])
+print("要素数は{}".format(len(scores)))
+print("合計は{}".format(sum(scores)))
+
+#scores[0] = 80
---
+++ code2_19.py
@@ -0,0 +1,5 @@
+members = ["松田"]
+scores = {"network":82}
+
+members = ("松田")
+print(type(members))
